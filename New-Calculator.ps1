8672c5bac4f83fe7d5fd024290a6c38e5dfe2b8b
Stef Harrington-Palmer <stefharrington-palmer@stefsloanermbp.bear.army>
2020-06-19 08:56:29 +0100

    quick tidyup

--- New-Calculator.ps1
+++ New-Calculator.ps1
@@ -1,31 +1,28 @@
-# inputs
-# Methods: if, switch, ifelse, ither...
-# Obfuscate, Make things more complicated, hex maybe?
-# Invoke-websession which ask.com's "what is x plus y" then scrapes
-# Something to foreach the operators
+# User "configuration"
+[CmdletBinding()]
+param (
+    $outputfile = "./SuperUseful-Calculator.ps1",
+    [int]$digits = 20
+)
 
-$outputfile = "./createdfile.ps1"
-
-$ifheader = "param (
+$header = "param (
     [int]`$inputNumberOne`,
     [int]`$inputNumberTwo`,
     [string]`$symbol
 )"
 
-[int]$digits = 20
 [int]$i = 1
 [int]$j = 1
 
-$ifheader | Out-File -path $outputfile 
+$header | Out-File -Path $outputfile 
 
 while ($i -le $digits) {
     while ($j -le $digits) {
-        add-content "if (`$inputNumberOne -eq $i -and `$inputNumberTwo -eq $j -and `$symbol -eq `"+`") { Write-Output $($i + $j) }" -path $outputfile 
-        add-content "if (`$inputNumberOne -eq $i -and `$inputNumberTwo -eq $j -and `$symbol -eq `"*`") { Write-Output $($i * $j) }" -path $outputfile 
-        add-content "if (`$inputNumberOne -eq $i -and `$inputNumberTwo -eq $j -and `$symbol -eq `"-`") { Write-Output $($i - $j) }" -path $outputfile 
+        Add-Content "if (`$inputNumberOne -eq $i -and `$inputNumberTwo -eq $j -and `$symbol -eq `"+`") { Write-Output $($i + $j) }" -Path $outputfile 
+        Add-Content "if (`$inputNumberOne -eq $i -and `$inputNumberTwo -eq $j -and `$symbol -eq `"*`") { Write-Output $($i * $j) }" -Path $outputfile 
+        Add-Content "if (`$inputNumberOne -eq $i -and `$inputNumberTwo -eq $j -and `$symbol -eq `"-`") { Write-Output $($i - $j) }" -Path $outputfile 
         $j++
     }
     $i++
     $j = 1
 }
-
---
+++ README.md
@@ -0,0 +1,20 @@
+# Calulate numbers
+
+Ever wished you had a super simple script that could add/subtract/multiply two numbers together?
+
+## Usage
+
+`./New-Calculator.ps1 -outputfile "./SuperUseful-Calculator.ps1" -digits 5`
+
+## Upcoming features (maybe)
+
+- [ ] Different methods
+- - [x] if
+- - [ ] elseif
+- - [ ] switch
+- - [ ] foreach?!
+- - [ ] try/catch
+- - [ ] while
+- - [ ] http with screen scraping?
+- [ ] Some obfuscation
+- - [ ] Hex?
